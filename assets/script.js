908e5838b37d4e0b9e801f90ab9e84cd0cf4f74b
Bad Boys Club <3ly.daley@gmail.com>
2022-05-03 19:05:39 -0700

    First bootstrap button functioning

--- assets/script.js
+++ assets/script.js
@@ -17,111 +17,118 @@ var quoteGardenUrl = "https://quote-garden.herokuapp.com/api/v3/quotes?per_page=
 var historyUrl = "http://history.muffinlabs.com/date?no-cors";
 
 
-
-var getThisDay = function () {
   
-  fetch(historyUrl).then(function(response){
-    response.json().then(function(data){
-
-      // variables to hold length of each set of arrays
-      var eventsLength = data.data.Events.length;
-      var deathLength = data.data.Deaths.length;
-      var birthLength = data.data.Births.length;
-      var thisDay = document.createElement('div');
-      
-      // for loops for each category, structured to recieve a random array selection on every refresh
-      // events category
-      for(var i = 0; i < 217; i++){
-        var numEvent = Math.floor(Math.random() * eventsLength);
+  var getThisDay = function () {
+    
+    fetch(historyUrl).then(function(response){
+      response.json().then(function(data){
         
-        var thisDayEvent = data.data.Events[numEvent].text;
-        var thisDayEventYear = data.data.Events[numEvent].year;
-
-
-      // Deaths category (so and so died on this day)
-        var numDeath = Math.floor(Math.random() * deathLength);
+        // variables to hold length of each set of arrays
+        var eventsLength = data.data.Events.length;
+        var deathLength = data.data.Deaths.length;
+        var birthLength = data.data.Births.length;
+        var thisDay = document.createElement('div');
         
-        var thisDayDeath= data.data.Deaths[numDeath].text;
-        var thisDayDeathYear = data.data.Deaths[numDeath].year;
-
+        // for loops for each category, structured to recieve a random array selection on every refresh
+        // events category
+        for(var i = 0; i < 217; i++){
+          var numEvent = Math.floor(Math.random() * eventsLength);
+          
+          var thisDayEvent = data.data.Events[numEvent].text;
+          var thisDayEventYear = data.data.Events[numEvent].year;
+          
+          
+          // Deaths category (so and so died on this day)
+          var numDeath = Math.floor(Math.random() * deathLength);
+          
+          var thisDayDeath= data.data.Deaths[numDeath].text;
+          var thisDayDeathYear = data.data.Deaths[numDeath].year;
+          
+          
+          //birth category
+          var numBirth = Math.floor(Math.random() * birthLength);
+          var thisDayBirth= data.data.Births[numBirth].text;
+          var thisDayBirthYear = data.data.Births[numBirth].year;
+          
+          
+          // HTML and append to div container hardcoded in index.HTML
+          // div container to hold data fetched in for loops
+          
+          thisDay.innerHTML =
+          "<p><b> An event of varying signifigance:</b> " + thisDayEvent + " ( " + thisDayEventYear + ") </br><b>" + 
+          thisDayBirth + "</b> was born on this day in <b>" + thisDayBirthYear + ". </b></br><b> " 
+          + thisDayDeath + "</b> died on this day in <b>" + thisDayDeathYear + ".</b></p>"
+          tdihContainer.appendChild(thisDay);
+          break; 
+        }  
         
-        //birth category
-        var numBirth = Math.floor(Math.random() * birthLength);
-        var thisDayBirth= data.data.Births[numBirth].text;
-        var thisDayBirthYear = data.data.Births[numBirth].year;
-        
-        
-        // HTML and append to div container hardcoded in index.HTML
-        // div container to hold data fetched in for loops
-
-        thisDay.innerHTML =
-        "<p><b> An event of varying signifigance:</b> " + thisDayEvent + " ( " + thisDayEventYear + ") </br><b>" + 
-        thisDayBirth + "</b> was born on this day in <b>" + thisDayBirthYear + ". </b></br><b> " 
-        + thisDayDeath + "</b> died on this day in <b>" + thisDayDeathYear + ".</b></p>"
-        tdihContainer.appendChild(thisDay);
-        break; 
-      }  
-      
-    });
-  });
-}
-
-
-var getQuotation = function() {
-
-  fetch(quoteGardenUrl).then(function(response) {
-    response.json().then(function(data) {
-      console.log(data);
-      
-      // gets random Quote from list and appends to parent Div
-      var famousQuote= document.createElement('div');
-      for (var i=0; i < data.data.length; i++ ){
-        // to generate a random number 
-        var random = Math.floor(Math.random() * data.data.length);
-        
-        var quote =  data.data[random].quoteText;
-        var author = data.data[random].quoteAuthor;
-        famousQuote.innerHTML ="<q>"+ quote +"</q></br><h4> -" + author + "</h4>"
-        quotationContainer.appendChild(famousQuote);
-        
-        // only need this loop to run one time
-        break;
-      }
-      
-    });
-  });
-}
-
-var getFunFact = function() {
-
-  fetch(funFactUrl).then(function(response) {
-    response.json().then(function(data) {
-      
-      // dont need random number for this data (already randomized)
-      var funFact= document.createElement('h4');
-      funFact.textContent = data;
-      funFactContainer.appendChild(funFact);
-
-    });
-  });
-}
-  
-var getAdvice = function() {
-
-  fetch(adviceUrl).then(function(response) {
-    response.json().then(function(data) {
-
-      // dont need random number for this data (already randomized)
-      document.querySelector('#adviceBtn').addEventListener("click", function(){
-        document.adviceModal.adviceModalBody.querySelector('#adviceModalBody').innerHTML= '<p>' + data.slip.advice + '</p>';
       });
-      adviceModal.hidden = "false";
     });
-  });
-}
+  }
+  
+  
+  var getQuotation = function() {
+    
+    fetch(quoteGardenUrl).then(function(response) {
+      response.json().then(function(data) {
+        console.log(data);
+        
+        // gets random Quote from list and appends to parent Div
+        var famousQuote= document.createElement('div');
+        for (var i=0; i < data.data.length; i++ ){
+          // to generate a random number 
+          var random = Math.floor(Math.random() * data.data.length);
+          
+          var quote =  data.data[random].quoteText;
+          var author = data.data[random].quoteAuthor;
+          famousQuote.innerHTML ="<q>"+ quote +"</q></br><h4> -" + author + "</h4>"
+          quotationContainer.appendChild(famousQuote);
+          
+          // only need this loop to run one time
+          break;
+        }
+        
+      });
+    });
+  }
+  
+  var getFunFact = function() {
+    
+    fetch(funFactUrl).then(function(response) {
+      response.json().then(function(data) {
+        
+        // dont need random number for this data (already randomized)
+        var funFact= document.createElement('h4');
+        funFact.textContent = data;
+        funFactContainer.appendChild(funFact);
+        
+      });
+    });
+  }
+  
+  var getAdvice = function() {
+    
+    fetch(adviceUrl).then(function(response) {
+      response.json().then(function(data) {
+        
+        console.log(data);
+        // dont need random number for this data (already randomized)
+        var advice = data.slip.advice;
+        
+        $('<p>' + advice + '</p>').appendTo($('#adviceModalBody'));
+        
+        $("#adviceModal").on('hidden.bs.modal', function(){
+          $("#adviceModalBody").html('');
+        });
+      });
+      
+    });
 
-document.querySelector('#adviceBtn').addEventListener("click", getAdvice); 
+  }
+    
+  
+  document.querySelector('#adviceBtn').addEventListener("click", getAdvice); 
+    
+    
+    
 
-
-
-getFunFact(); getQuotation(); getThisDay()
